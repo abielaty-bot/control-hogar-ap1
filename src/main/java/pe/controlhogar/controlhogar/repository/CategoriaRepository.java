81776828d947b6eea277708621bc807565e27afe
Abiel Tineo <abieltineoyupanqui@gmail.com>
2026-08-28 06:24:32 -0500

    Listar solo categorias activas con Query Method

--- src/main/java/pe/controlhogar/controlhogar/repository/CategoriaRepository.java
+++ src/main/java/pe/controlhogar/controlhogar/repository/CategoriaRepository.java
@@ -1,8 +1,13 @@
 package pe.controlhogar.controlhogar.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import pe.controlhogar.controlhogar.entity.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+public interface CategoriaRepository
+        extends JpaRepository<Categoria, Long> {
 
+    List<Categoria> findByActivoTrueOrderByNombreAsc();
 }
